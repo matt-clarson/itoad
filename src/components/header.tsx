d5604eb3a080804efdb70c86f45d47c5461bfc78
matt-clarson <clarson469@googlemail.com>
2020-12-21 10:50:27 +0000

    added layout styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,13 +3,16 @@ import { Link } from "gatsby";
 
 type HeaderProps = {
     siteTitle?: string;
+    className?: string;
 };
 
-export const Header: FC<HeaderProps> = ({ siteTitle }) => (
-    <header>
+export const Header: FC<HeaderProps> = ({ siteTitle, className }) => (
+    <header className={className}>
         <div>
-            <h1 className="font-title text-4xl underline max-w-sm">
-                <Link to="/">{siteTitle ?? ""}</Link>
+            <h1 className="font-title text-4xl max-w-sm">
+                <Link className="hover:underline" to="/">
+                    {siteTitle ?? ""}
+                </Link>
             </h1>
         </div>
     </header>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 import { Header } from "./header";
 import "./layout.css";
@@ -17,13 +17,49 @@ export const Layout: FC = ({ children }) => {
 
     return (
         <>
-            <div className="w-full min-h-full flex flex-row justify-between">
-                <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-                <div>
-                    <main>{children}</main>
+            <div className="py-4 pl-6 w-full min-h-screen">
+                <div className="flex flex-row justify-between">
+                    <div className="flex flex-col">
+                        <Header
+                            className="ml-5"
+                            siteTitle={data.site.siteMetadata?.title || `Title`}
+                        />
+                        <span
+                            role="presentation"
+                            className="inline-block h-1 w-5/12 bg-gray-800 mt-3 mb-6"
+                        />
+                        <nav className="ml-5">
+                            <ul className="font-text text-lg">
+                                <li className="hover:underline">
+                                    <Link to="/">{"Home"}</Link>
+                                </li>
+                                <li className="hover:underline">
+                                    <Link to="/recipes">{"Recipes"}</Link>
+                                </li>
+                                <li className="hover:underline">
+                                    <a href="https://www.instagram.com/itsthisoradoughnut">
+                                        {"@itsthisoradoughnut"}
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div>
+                        <main>{children}</main>
+                    </div>
                 </div>
+                <footer className="font-text">
+                    <p>{`© ${new Date().getFullYear()}`}</p>
+                    <p>
+                        <a
+                            className="font-bold tracking-wide"
+                            href="https://github.com/matt-clarson"
+                        >
+                            {`Built with ❤️ in Manchester, UK`}
+                        </a>
+                    </p>
+                </footer>
             </div>
-            <footer>{`© ${new Date().getFullYear()}, Built with ❤️ in Manchester, UK`}</footer>
         </>
     );
 };
